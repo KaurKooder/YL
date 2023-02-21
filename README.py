94e802fe2b70387002778f086d5ed7f69720dee6
KaurKooder <109687242+KaurKooder@users.noreply.github.com>
2023-02-21 18:43:15 +0200

    Add files via upload

---
+++ README.py
@@ -0,0 +1,11 @@
+#git status saad versiooni failide olekut kontrollida
+#git add <fail>  lisa fail uude versiooni
+#git commit -m  "readme versioon 2"
+#git pull -- lae kõik koodi muudatused alla
+#git branch -- kuvab kõik branchi'id
+#git branch nt:KaurBranch -- loob uue
+#git checkout KaurBranch -- viib koodi uuele branchile
+#git push --set-upstream origin KaurBranch -- uus branch githubi
+
+#git checkout main
+#git merge <branch name>
